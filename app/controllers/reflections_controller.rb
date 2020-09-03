cddaac80370b756fd2aa5bcb7407d8d3ad98967f
ysktlv <k109tkms@gmail.com>
2020-09-03 18:18:54 +0900

    日記編集

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,5 +1,6 @@
 class ReflectionsController < ApplicationController
   before_action :move_to_login
+  before_action :set_reflection, only: [:show, :edit, :update, :destroy]
 
   def index
     @reflections = Reflection.all
@@ -7,18 +8,30 @@ class ReflectionsController < ApplicationController
 
   def create
     @reflection = Reflection.new(reflection_params)
+
     if @reflection.save
       redirect_to reflection_path(@reflection)
     else
-      render :new
+      render :index
     end
   end
 
   def show
-    @reflection = Reflection.find(params[:id])
     @points = []
     @reflection.goal_reflections.each do |goal_reflection|
       @points.push(goal_reflection.goal.point)
+    end
+  end
+
+  def edit
+    redirect_to root_path unless logged_in? && current_user.id == @reflection.user_id
+  end
+
+  def update
+    if @reflection.update(edit_reflection_params)
+      redirect_to reflection_path(@reflection)
+    else
+      render :edit
     end
   end
 
@@ -29,6 +42,14 @@ class ReflectionsController < ApplicationController
   end
 
   def reflection_params
-    params.permit(:start_time, :impression, :user_id, {goal_ids: []})
+    params.permit(:start_time, :impression, :user_id, {goal_ids: []}).merge(user_id: current_user.id)
+  end
+
+  def edit_reflection_params
+    params.require(:reflection).permit(:start_time, :impression, :user_id, {goal_ids: []}).merge(user_id: current_user.id)
+  end
+
+  def set_reflection
+    @reflection = Reflection.find(params[:id])
   end
 end
